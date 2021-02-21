e31fc24a9a2a18d549913809b2586503f8ad4e89
guoqiiii <463008393@qq.com>
2021-02-21 19:00:16 +0800

    完善【创建班级】页UI

--- src/pages/create_class/create_class.vue
+++ src/pages/create_class/create_class.vue
@@ -22,18 +22,30 @@
       <div class="block_title">通用功能</div>
       <div class="block">
         <div class="line">
-          <div class="base_info_name" style="width:112px;">
+          <div class="base_info_name" style="width: auto">
             导入其他班级学生
           </div>
           <div class="base_info_ipt"><input type="text" /></div>
         </div>
         <div class="line">
-          <div class="base_info_name" style="width:182px;">
-            允许通过搜索框搜索到该班级
+          <div class="base_info_name" style="width: auto">
+            是否可以通过搜索框搜索到该班级
           </div>
-          <div class="switch">
-            <span class="on">开</span>
-            <span class="off">关</span>
+          <div
+            @click="toggleSearch"
+            :style="{
+              backgroundColor: canSearch ? '#1c7eff' : '#ddd',
+              borderColor: canSearch ? '#1c7eff' : '#ddd',
+            }"
+            class="switch"
+          >
+            <span
+              :style="{
+                transform: canSearch
+                  ? 'translate(100%, -50%)'
+                  : 'translate(3px, -50%)',
+              }"
+            />
           </div>
         </div>
       </div>
@@ -46,7 +58,17 @@
 import ZsButtonVue from "../../components/zs_button/ZsButton.vue";
 import ZsNavBarVue from "../../components/zs_nav_bar/ZsNavBar.vue";
 export default {
-  components: { zsNavBar: ZsNavBarVue, zsButton: ZsButtonVue }
+  components: { zsNavBar: ZsNavBarVue, zsButton: ZsButtonVue },
+  data() {
+    return {
+      canSearch: true
+    };
+  },
+  methods: {
+    toggleSearch() {
+      this.canSearch = !this.canSearch;
+    }
+  }
 };
 </script>
 
