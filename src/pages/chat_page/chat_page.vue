3653e324892fe3bc4bd5a73c388d335a73b6982f
guoqiiiii <463008393@qq.com>
2021-03-29 20:09:26 +0800

    对接socket.io消息推送

--- src/pages/chat_page/chat_page.vue
+++ src/pages/chat_page/chat_page.vue
@@ -2,48 +2,72 @@
   <div class="chat_page">
     <zs-nav-bar title="软件1班"></zs-nav-bar>
     <div class="message_box">
-      <div class="message">
-        <div class="avatar"><img src="@/images/default_avatar.png" alt=""></div>
-        <div class="block">
-          <div class="info">王小明 2021/03/20</div>
-          <div class="msg">老师对不起，我睡过头了！</div>
+      <template v-for="(msg, idx) in msgList">
+        <div :key="idx" v-if="msg.userId !== userInfo.id" class="message">
+          <div class="avatar"><img src="@/images/default_avatar.png" alt=""></div>
+          <div class="block">
+            <div class="info">{{msg.realName}} {{new Date(msg.createTime).Format('yyyy/MM/dd hh:mm:ss')}}</div>
+            <div class="msg">{{msg.msg}}</div>
+          </div>
         </div>
-      </div>
-      <div class="message">
-        <div class="avatar"><img src="@/images/default_avatar.png" alt=""></div>
-        <div class="block">
-          <div class="info">王小明 2021/03/20</div>
-          <div class="msg">老师对不起，我睡过头了！老师对不起，我睡过头了！老师对不起，我睡过头了！</div>
+        <div :key="idx" v-else class="message me">
+          <div class="block">
+            <div class="info">{{msg.realName}} {{new Date(msg.createTime).Format('yyyy/MM/dd hh:mm:ss')}}</div>
+            <div class="msg">{{msg.msg}}</div>
+          </div>
+          <div class="avatar"><img src="@/images/default_avatar.png" alt=""></div>
         </div>
-      </div>
-
-      <div class="message me">
-        <div class="block">
-          <div class="info">王小明 2021/03/20</div>
-          <div class="msg">老师对不起，我睡过头了！老师对不起，我睡过头了！老师对不起，我睡过头了！老师对不起，我睡过头了！</div>
-        </div>
-        <div class="avatar"><img src="@/images/default_avatar.png" alt=""></div>
-      </div>
+      </template>
     </div>
     <div class="foot_bar">
-      <div class="input"><textarea /></div>
-      <div class="button">发送</div>
+      <div class="input"><textarea v-model="chatMsg" /></div>
+      <div @click="sendMsg" @keypress.enter="sendMsg" class="button">发送</div>
     </div>
   </div>
 </template>
 
 <script>
 import ZsNavBarVue from '../../components/zs_nav_bar/ZsNavBar.vue';
+import UserAPI from '../../api/userAPI';
 export default {
   components: { zsNavBar: ZsNavBarVue },
   data() {
     return {
-      roomId: ""
+      roomId: "",
+      userInfo: {},
+      chatMsg: "",
+      msgList: []
     };
   },
   mounted() {
-    this.roomId = this.$route.query.roomId;
-    this.$socket.emit('joinRoom', this.roomId);
+    UserAPI.getMyUserInfo().then(({data}) => {
+      this.userInfo = data;
+      this.roomId = this.$route.query.roomId;
+      this.$socket.emit('joinRoom', this.roomId);
+      this.$socket.on('receive_chat_msg', (msgVo) => {
+        this.msgList.push(JSON.parse(msgVo));
+      });
+    });
+  },
+  beforeDestroy() {
+    this.$socket.emit('leaveRoom', this.roomId);
+  },
+  methods: {
+    sendMsg() {
+      if(!this.chatMsg) {
+        return;
+      }
+      const msgVo = {
+        roomId: this.roomId,
+        userId: this.userInfo.id,
+        realName: this.userInfo.realName,
+        createTime: new Date(),
+        msg: this.chatMsg
+      };
+      this.msgList.push(msgVo);
+      this.$socket.emit('send_chat_msg', JSON.stringify(msgVo));
+      this.chatMsg = "";
+    }
   }
 };
 </script>
